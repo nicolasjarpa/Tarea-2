11c19ae86494f2925c5bf6ac153dee1862af71e9
BuildTools <pegajardo2021@udec.cl>
2022-10-18 19:59:20 -0300

    Tarea2/src/tarea2/Tarea2.java

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -1,18 +1,1 @@
 
-package tarea2;
-class Expendedor{
-    
-    public Expendedor(){
-        
-    }
-}
-
-
-public class Tarea2 {
-
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
