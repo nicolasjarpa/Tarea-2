c097754019850b6c5b9a7790eef887c89e8cdc00
BuildTools <pegajardo2021@udec.cl>
2022-10-18 19:49:01 -0300

    tt

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package tarea2;
 
-/**
- *
- * @author Nicolás Jarpa
- */
+package tarea2;
+class Expendedor{
+    
+    public Expendedor(){
+        
+    }
+}
+
+
 public class Tarea2 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
