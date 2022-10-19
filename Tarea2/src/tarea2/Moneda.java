9370b096c8b27a289ff6f97ddc05a178459b1457
BuildTools <pegajardo2021@udec.cl>
2022-10-18 21:21:17 -0300

    Tarea2/src/tarea2/Moneda.java

--- Tarea2/src/tarea2/Moneda.java
+++ Tarea2/src/tarea2/Moneda.java
@@ -1,6 +1,8 @@
 
 package tarea2;
 
+import java.util.ArrayList;
+
 abstract class Moneda{
     private Moneda m;
 public Moneda(){
@@ -24,5 +26,27 @@ public Moneda(){
  class Moneda1000 extends Moneda{
      public int getValor(){
         return 1000;
+    }
+}
+
+class DepositoVuelto{
+    private ArrayList <Moneda> m;
+    private int total;
+    private Moneda1000 a;
+    private Moneda500 b;
+    private Moneda100 c;
+    
+    public DepositoVuelto(){
+      m = new ArrayList <Moneda>();
+      total = a.getValor()+b.getValor()+c.getValor();
+    }
+    public void addMoneda(Moneda b){
+        m.add(b);
+    }
+    public Moneda vuelto(){
+        if(total == 0){
+            return null;
+        }
+        return m.remove(0);
     }
 }
