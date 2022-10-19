d79a1174bf7c333e461f718562989780ef846485
BuildTools <pegajardo2021@udec.cl>
2022-10-18 21:02:53 -0300

    Tarea2/src/tarea2/Deposito.java

--- Tarea2/src/tarea2/Deposito.java
+++ Tarea2/src/tarea2/Deposito.java
@@ -1,9 +1,69 @@
 
 package tarea2;
 import java.util.ArrayList;
-public class Deposito{
-    
-    public Deposito(){
+
+
+package tarea2;
+
+
+abstract class Bebida {
+ private int xd;
+    public Bebida(int a){
+        xd=a;
         
+    }
+    public int getSerie(){
+        return xd;
+    }
+    public String beber(){
+        return "sabor: ";
+    }
+}
+
+class Fanta extends Bebida{
+        public Fanta(int a){
+    super(a);
+}
+public String beber(){
+    
+return super.beber()+"Fanta";
+}
+}
+
+
+class Coca extends Bebida{
+ public Coca(int a){
+    super(a);
+}
+public String beber(){
+    
+return super.beber()+"coca";
+}
+}
+
+
+class Sprite extends Bebida{
+   public Sprite(int a){
+    super(a);
+}
+public String beber(){
+    
+return super.beber()+"sprite";
+}
+}
+
+class Deposito{
+    private ArrayList <Bebida> n;
+    public Deposito(){
+      n = new ArrayList <Bebida>();
+    }
+    public void addBebida(Bebida b){
+        n.add(b);
+    }
+    public Bebida getBebida(){
+        if(n.size() ==0){
+            return null;
+        }
+        return n.remove(0);
     }
 }
