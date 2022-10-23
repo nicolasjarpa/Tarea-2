f8e2c96422506fea424e40650270348cd0c185cb
unknown <nijarpa2021@udec.cl>
2022-10-23 19:19:22 -0300

    Tarea2/src/tarea2/Tarea2.java

--- Tarea2/src/tarea2/Expendedor.java
+++ Tarea2/src/tarea2/Expendedor.java
@@ -2,8 +2,19 @@ package tarea2;
 import java.util.ArrayList;
 public class Expendedor {
     private Deposito coca;
-    public Expendedor(){
-        
+    private Deposito fanta;
+    private Deposito sprite;
+    public Expendedor(int cantidad, int precio){
+        coca= new Deposito();
+            fanta = new Deposito();
+          sprite = new Deposito();
+           for(int i=0; i<cantidad; i++){
+              coca.addBebida(new Coca(i));
+              fanta.addBebida(new Fanta(i));
+              sprite.addBebida(new Sprite(i));
+              }
+           
+      }
     }
     
-}
+
